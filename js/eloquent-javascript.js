39bbfb489a5dbaf6cb0dade0bed7277afb8781f6
it-Hooligan <57491204+it-Hooligan@users.noreply.github.com>
2020-11-14 18:08:42 +0200

    Initial commit

---
+++ js/eloquent-javascript.js
@@ -0,0 +1,32 @@
+ //Теория 
+
+ //- Цикл while
+ var result = 1;
+ var counter = 0;
+ while (counter < 10) { //проходит  условие и потом решает выполнять тело цикла или не или остановить
+     result = result * 2;
+     counter = counter + 1;
+ }
+ console.log(result);
+
+
+
+
+
+
+
+
+
+
+
+
+ //Домашка 
+
+ let count = 0;
+ let x = '#';
+
+ while (count <= 10) {
+     console.log(x);
+     x = x + '#';
+     count = count + 1;
+ }
